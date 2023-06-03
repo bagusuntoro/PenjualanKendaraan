0f0ad6eeee48d9f66da0565c4aa2e7fd4531c321
bagusuntoro <untorobagus65@gmail.com>
2023-06-03 11:09:02 +0700

    Make migration

---
+++ database/migrations/2023_06_03_034729_create_kendaraans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKendaraansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kendaraans', function (Blueprint $collection) {
+            $collection->id();
+            $collection->date('tahun_keluaran');
+            $collection->string('warna');
+            $collection->int('harga');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kendaraans');
+    }
+}
